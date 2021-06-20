85d730a6e3ea3ec3f4c26159f777e84c85fd58eb
Dhyan961 <79209452+Dhyan961@users.noreply.github.com>
2021-06-20 19:52:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 canvas = document.getElementById("myCanvas");
-cntx = canvas.getcontext("2d");
+cntx = canvas.getContext("2d");
 
 car1_width = 120;
 car1_height = 70;
@@ -7,11 +7,11 @@ car1_img = "car1.png";
 car1_x = 10;
 car1_y = 10;
 
-car1_width = 120;
-car1_height = 70;
-car1_img = "car1.png";
-car1_x = 10;
-car1_y = 100;
+car2_width = 120;
+car2_height = 70;
+car2_img = "car2.png";
+car2_x = 10;
+car2_y = 100;
 
 background_image = "racing.jpg"
 
@@ -23,13 +23,21 @@ function add1(){
     car1_imgTag = new Image();
     car1_imgTag.onload = uploadcar1;
     car1_imgTag.src = car1_img;
+
+    car2_imgTag = new Image();
+    car2_imgTag.onload = uploadcar2;
+    car2_imgTag.src = car2_img;
 }
 
 function uploadBackground(){
-    cntx.drawImage(background_imageTag,0,0,canvas.width,canvas.hieght);
+    cntx.drawImage(background_imageTag,0,0,canvas.width,canvas.height);
 }
 function uploadcar1(){
     cntx.drawImage(car1_imgTag,car1_x,car1_y,car1_width,car1_height);
+}
+
+function uploadcar2(){
+    cntx.drawImage(car2_imgTag,car2_x,car2_y,car2_width,car2_height);
 }
 
 window.addEventListener("keydown",my_keydown);
